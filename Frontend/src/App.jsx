59614c3833bc570975247cca9e09343058283494
Ridima28 <ridimafinal@gmail.com>
2026-06-19 17:50:54 +0530

    fix: added a new logo as last one's text wasn't visible

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div className="bg-white text-black dark:bg-gradient-to-b dark:from-[#242124] dark:to-[#000000] dark:text-white">
+    <div className="bg-white text-black ">
       <div className='flex h-screen'>
         <SideBar />
         <Routes>
